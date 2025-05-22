1dfe099304dc5214c77fb6dd666aab67c2b2b5e6
zakhan14 <zuka.xantadze@gmail.com>
2025-05-22 19:06:11 +0200

    1.5

--- FITTRACK/FITTRACK/settings.py
+++ FITTRACK/FITTRACK/settings.py
@@ -81,9 +81,19 @@ WSGI_APPLICATION = 'FITTRACK.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
