1d43e226dc451d321ba3b6885ab829305548176a
zakhan14 <zuka.xantadze@gmail.com>
2025-05-23 15:12:29 +0200

    2.1

--- FITTRACK_project/fittrack/settings.py
+++ FITTRACK_project/fittrack/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_m-lpnvylzh055aaq@@l)x(evcga6%n+@-(f5&tf-^z-^z@p_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
